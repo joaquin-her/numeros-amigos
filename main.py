ed5d711fc6bc65b03c5907a951e4929335e80a3e
Joaquin H <hernandez.joaquin346@gmail.com>
2026-03-20 10:34:13 -0300

    mejora: memoization

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import time
 def amigos(MAX):
-    
+    sumas = []
     t1=time.time()
     for i in range(MAX):
         s=0
         for j in range (1,i-1+1):
             if i%j==0:
                 s+=j
-        s2=0
-        for k in range (1,s-1):
-            if s%k==0:
-                s2+=k
-        if i==s2:
-            print(i,s)
-   
+        sumas.append(s)
+        if s > i:
+            continue
+        else:
+            s2 = sumas[s]
+            if i == s2:
+                print(i,s)       
     t2=time.time()    
     print(t2-t1)
     
