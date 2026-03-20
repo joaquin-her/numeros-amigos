a6e1b38ac1d1d215d95c40b292954388451b795c
Joaquin H <hernandez.joaquin346@gmail.com>
2026-03-20 11:10:26 -0300

    mejora: uso raiz para hallar divisores

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import time
+import math
 def amigos(MAX):
-    sumas = []
+    sumas = [0] * MAX
     t1=time.time()
     for i in range(MAX):
-        s=0
-        for j in range (1,i-1+1):
-            if i%j==0:
-                s+=j
-        sumas.append(s)
+        sumas[i] = suma_de_divisores(i)
+        s = sumas[i]
         if s > i:
             continue
         else:
-            s2 = sumas[s]
-            if i == s2:
-                print(i,s)       
+            if s < MAX:
+                s2 = sumas[s]
+                if i == s2:
+                    print(i,s)       
     t2=time.time()    
     print(t2-t1)
     
+def suma_de_divisores(numero):
+    if numero <= 1:
+        return 0
+    suma = 1  # 1 siempre es divisor
+    sqrt_n = int(math.sqrt(numero))
+    
+    for i in range(2, sqrt_n + 1):
+        if numero % i == 0:
+            suma += i
+            if i != numero // i:  # Evitar contar dos veces el mismo divisor
+                suma += numero // i
+    
+    return suma
 amigos(100000)
