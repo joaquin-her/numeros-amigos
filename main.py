72b50de2dc71b3dba748e566754e8f877cf96e88
Joaquin H <hernandez.joaquin346@gmail.com>
2026-03-22 22:26:41 -0300

    reroll algoritm

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import time
-import math
+
 def amigos(MAX):
     sumas = {}
-    t1=time.time()
-    if MAX > 0:
-        print(0, 0)
-    # Calcular sumas de divisores usando el método de la criba
+    t1 = time.time()
+    if MAX >= 0:
+        print(0,0)
+    # Calcular sumas de divisores usando el metodo de la criba
     for i in range(1, MAX):
         for j in range(i * 2, MAX + 1, i):
             sumas[j] = sumas.get(j, 0) + i
     
+    # Buscar pares de numeros amigos
+    for i in range(MAX):
         s = sumas.get(i, 0)
         if s > i:
             continue
@@ -18,7 +20,5 @@ def amigos(MAX):
                 s2 = sumas[s]
                 if i == s2:
                     print(i, s)       
-    t2=time.time()    
-    print(t2-t1)
-    
-amigos(200000)
+    t2 = time.time()
+    print(t2 - t1)
